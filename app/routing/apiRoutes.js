62dee4766fb4c4491cadb731c9f9b96da9b7d320
Dana Cronin <cronin43224235@gmail.com>
2019-12-15 14:49:31 -0500

    moving comparison logic out and into to survey, also  adding an app.post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,18 @@
 let userData = require("../data/friends");
 
-module.exports = function (app) {
+module.exports = function(app) {
+    console.log(`api route connecting?`);
 
     app.get("/api/friends", function (req, res) {
         res.json(userData);
-
-        res.json("#modal", compare(userData))
-    });
-}
-
-function compare(array) {
-    let user = array[array.length - 1];
-    let friends = [];
-    array.forEach(e => {
-        let diff = 0;
-        for (i = 0; i < 10; i++) {
-            diff += Math.abs(user.scores[i] - e.scores[i]);
-        }
-        friends.push(diff);
+        console.log(`exports-from-api-routes: ${userData}`)
+        // res.json("#modal", compare(userData))
     });
 
-    for (k = 0; k < 50; k++) {
-        for (j = 0; j < friends.length; j++){
-            if (friends[j] == k) {
-                return array[j]
-            }
-        }
-    }
+    app.post("/api/friends", function(req, res) {
+        console.log(`push?`);
+        userData.push(req.body);
+        res.json(true);
 
-    return { name: "Error", image: "https://cdn.shopify.com/s/files/1/0322/6897/files/404-permalink.png?432866230176278629", scores: [404] }
-
-    // Display as MODAL POP UP (name and picture)
+    })
 }

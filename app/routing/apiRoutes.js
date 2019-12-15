84b4f48c28b410711829778e6ca185011d02ad9e
Dana Cronin <cronin43224235@gmail.com>
2019-12-15 16:42:43 -0500

    erasing comments and console logs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,12 @@
 let userData = require("../data/friends");
 
 module.exports = function(app) {
-    console.log(`api route connecting?`);
-
     app.get("/api/friends", function (req, res) {
         res.json(userData);
-        console.log(`exports-from-api-routes: ${userData}`)
-        // res.json("#modal", compare(userData))
     });
 
     app.post("/api/friends", function(req, res) {
-        console.log(`push?`);
         userData.push(req.body);
         res.json(true);
-
     })
 }

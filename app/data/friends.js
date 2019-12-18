58e18939edb5994cde8d286d810b9f844389cce6
Dana Cronin <cronin43224235@gmail.com>
2019-12-18 15:44:42 -0500

    changing admin scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ let userData = [
     {
         "name" : "Administration",
         "photo" : "https://www.seppic.com/sites/seppic/files/styles/retina_cover_page/public/2016/11/18/banner-route-of-administration-cover-page.jpg?itok=nSZr9-tb",
-        "scores" : [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+        "scores" : [5, 5, 5, 5, 5, 5, 5, 5, 5, 5] //Administration Disagrees with Everything?
     }
 ];
 
